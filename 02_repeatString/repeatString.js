a1713d523f1c2fa6c7e1de6e0833a340b69c5df1
nm-woodward <nowoodward15@gmail.com>
2022-02-21 11:21:48 -0800

    Completed exercises #2/#3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    let finalString = '';
+
+    if(repeats < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < repeats; i++) {
+        finalString += string;
+    }
+
+    return finalString;
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,21 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+
+    let stringArray = inputString.split(" ");
+
+    let finalString = '';
+
+    stringArray.forEach((element) => {
+        let elementSplit = element.split("");
+        elementSplit = elementSplit.reverse();
+        let elementFinal = elementSplit.join("");
+        finalString = finalString + ' ' + elementFinal;
+      });
+
+      finalString = finalString.split(" ");
+      finalString = finalString.reverse("");
+      finalString = finalString.join(" ").trim();
+
+    return finalString;
 
 };
 
